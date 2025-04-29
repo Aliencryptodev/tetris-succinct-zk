52a27b5fe618b7d32d0daabae6aa7a9cfda64015
Aliencryptodev <aliencryptodev@gmail.com>
2025-04-29 02:28:24 +0200

    Update tetris.js

--- frontend/tetris.js
+++ frontend/tetris.js
@@ -170,9 +170,15 @@ function update(time = 0) {
 }
 
 function updateScore() {
-    document.getElementById('scoreTable').querySelector('tbody').innerHTML =
-        `<tr><td>${playerName}</td><td>${player.score}</td></tr>`;
+    if (gameOver) {
+        document.getElementById('scoreTable').querySelector('tbody').innerHTML =
+            `<tr><td>YOU</td><td>${finalScore}</td></tr>`;
+    } else {
+        document.getElementById('scoreTable').querySelector('tbody').innerHTML =
+            `<tr><td>YOU</td><td>${player.score}</td></tr>`;
+    }
 }
+
 
 function saveScore() {
     let scores = JSON.parse(localStorage.getItem('topScores')) || [];
