d48b41d82d9e3026ed16362d38cb63d9bbe8514a
Aliencryptodev <aliencryptodev@gmail.com>
2025-05-01 17:24:22 +0200

    Add files via upload

--- frontend/sound.js
+++ frontend/sound.js
@@ -1,5 +1,5 @@
-// sound.js
 
+// sound.js
 const music = new Audio('https://raw.githubusercontent.com/Aliencryptodev/tetris-succinct-zk/main/assets/music.mp3');
 music.preload = "auto";
 music.loop = true;
@@ -16,32 +16,23 @@ function canPlayMusic() {
     return music.readyState >= 2;
 }
 
-// Función para iniciar la música
 function playMusic() {
     if (canPlayMusic()) {
         music.currentTime = 0;
-        music.play().catch(e => {
-            console.log('Error al reproducir la música:', e);
-        });
-    } else {
-        console.log('No se puede reproducir música ahora.');
+        music.play().catch(e => console.log('Error al reproducir la música:', e));
     }
 }
 
-// Función para pausar la música
 function pauseMusic() {
     music.pause();
 }
 
-// Reproducir sonido al romper una fila
 function playLineClearSound() {
     clearSound.currentTime = 0;
     clearSound.play().catch(err => console.warn('No se pudo reproducir rompefila:', err));
 }
 
-// Reproducir sonido en Game Over
 function playGameOverSound() {
     gameOverSound.currentTime = 0;
     gameOverSound.play().catch(err => console.warn('No se pudo reproducir gameover:', err));
 }
-
